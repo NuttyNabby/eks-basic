b273e0948055f11ee5a33ddd02a21fe83dd049fa
kento555 <87764527+Kento555@users.noreply.github.com>
2025-05-14 17:25:16 +0800

    update to avengers

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce9-tfstate" #Update accordingly
-    key    = "ws-eks.tfstate"   #Update accordingly
+    key    = "avengers-eks.tfstate"   #Update accordingly
     region = "us-east-1"        #Update accordingly
   }
 }
