2b5d595b44f3b3cf206181e32527565d0c2a3694
NuttyNabby <nabilah.tang@gmail.com>
2025-06-09 19:07:16 +0800

    fixed bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "nab-sctp-ce9-tfstate" #Update accordingly
+    bucket = "nab-sctp-ce9-tfstate" #Edit accordingly
     key    = "nabilah-ce9.tfstate"   #Update accordingly
     region = "us-east-1"        #Update accordingly
   }
